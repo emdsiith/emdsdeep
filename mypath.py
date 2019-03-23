c2ad1940aa2d5857a16b80309093218476324588
Nageswara Rao <cs17emds11013@iith.ac.in>
2019-03-23 13:23:00 +0530

    datasets paths fix
    Added envron variable support
    Changes default sbd dataset availability to false

--- mypath.py
+++ mypath.py
@@ -1,14 +1,16 @@
+import os
+
 class Path(object):
     @staticmethod
     def db_root_dir(dataset):
         if dataset == 'pascal':
-            return '/path/to/datasets/VOCdevkit/VOC2012/'  # folder that contains VOCdevkit/.
+            return os.getenv('PASCAL_DATASET_PATH', '..\\..\\datasets\\pascal-voc-2012\\VOCdevkit\\VOC2012\\')  # folder that contains VOCdevkit/.
         elif dataset == 'sbd':
-            return '/path/to/datasets/benchmark_RELEASE/'  # folder that contains dataset/.
+            return os.getenv('SBD_DATASET_PATH','/path/to/datasets/benchmark_RELEASE/')  # folder that contains dataset/.
         elif dataset == 'cityscapes':
-            return '/path/to/datasets/cityscapes/'     # foler that contains leftImg8bit/
+            return os.getenv('CITYSCAPES_DATASET_PATH', '/path/to/datasets/cityscapes/')     # foler that contains leftImg8bit/
         elif dataset == 'coco':
-            return '..\coco-data'
+            return os.getenv('COCO_DATASET_PATH','..\coco-data')
         else:
             print('Dataset {} not available.'.format(dataset))
             raise NotImplementedError
